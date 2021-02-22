096cb2f4ae60150488ae0c22419d8f2e371041d7
Dane Murphy <danem7@gmail.com>
2021-02-21 16:58:52 -0800

    removed getLog for endpoint /api/log/:taskId

--- src/redux/slices/log/helpers.ts
+++ src/redux/slices/log/helpers.ts
@@ -3,7 +3,6 @@ import { AlertConstants } from '../alerts/AlertConstants';
 import { LogFormData } from '../../../components/forms/types';
 import { IAlert } from '../alerts/types';
 import { getTokens } from '../auth/helpers';
-import { ILog } from './types';
 
 export async function updateLog(id: number, formData: LogFormData) {
     const { token } = getTokens();
@@ -25,15 +24,4 @@ export async function deleteLogItem(id: number) {
     });
 
     return { ...res.data, type: AlertConstants.Warning };
-}
-
-export async function getLog(taskId: number) {
-    const { token } = getTokens();
-    const res: AxiosResponse<ILog[]> = await axios.get(`/api/logs/${taskId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-
-    return res.data;
 }
