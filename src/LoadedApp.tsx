8f57709664003df0614aacdc9e61534606d6c8c3
Dane Murphy <danem7@gmail.com>
2021-02-22 19:40:33 -0800

    verify account route good

--- src/LoadedApp.tsx
+++ src/LoadedApp.tsx
@@ -5,6 +5,7 @@ import Register from './components/authentication/register/Register';
 import Login from './components/authentication/login/Login';
 import SnackBarComponent from './components/ui/SnackBar';
 import Layout from './components/pages/common/Layout';
+import Verify from './components/authentication/verify/Verify';
 import { Route, Switch } from 'react-router-dom';
 import { IAlert } from './redux/slices/alerts/types';
 
@@ -12,17 +13,18 @@ interface ILoadedAppProps {
     alerts: IAlert[];
 }
 
-const LoadedApp: React.FC<ILoadedAppProps> = (props): JSX.Element => (
+const LoadedApp: React.FC<ILoadedAppProps> = ({ alerts }): JSX.Element => (
     <Fragment>
         <Switch>
             <Route exact path={'/register'} component={Register} />
             <Route exact path={'/login'} component={Login} />
+            <Route exact path={'/verify/:hash'} component={Verify} />
             <Layout>
                 <Route exact path={'/'} component={Dashboard} />
                 <Route exact path={'/archive'} component={Archive} />
             </Layout>
         </Switch>
-        {props.alerts.map((alert: IAlert, i: number) => (
+        {alerts.map((alert: IAlert, i: number) => (
             <SnackBarComponent
                 key={i}
                 message={alert.message}
